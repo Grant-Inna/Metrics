61a85928cccde9a6e944e89451dc1ffd6103e45d
Инна Грант <grant@SMS.SMS>
2017-08-22 14:43:16 +0300

    Our script for uploaded files

--- app/js/specialScript.js
+++ app/js/specialScript.js
@@ -10,3 +10,25 @@ $(function(){
 
 });
 
+function myFunction(){
+    var x = document.getElementById("uploadFile");
+    var txt = "";
+    if ('files' in x) {
+        if (x.files.length == 0) {
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                var file = x.files[i];
+                if ('name' in file) {
+                    txt += "<div class=\"uploadedFiles__oneFile\">" + file.name;
+                }
+                if ('size' in file) {
+                    var a = file.size;
+                    var size = a * 0.001;
+                    txt += " (" + size + " Кб) </div>";
+                }
+            }
+        }
+    }
+
+    document.getElementById("files").innerHTML = txt;
+}
